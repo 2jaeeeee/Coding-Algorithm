aa6367fc403b7585151151e4b05399ff68f59c75
2jae <qkrdmstydtyd@naver.com>
2022-01-13 23:11:50 +0900

    리트코드 문제 46: 그래프 풀이

---
+++ LeetCode - 46/LeetCode - 46 - 풀이2.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        prev = []
+
+        def dfs(elements):
+            #순열이 완성되면 결과에 저장
+            if len(elements) == 0:
+                results.append(prev[:])
+
+            #입력한 요소들 중 하나씩 dfs 호출
+            for e in elements:
+                next = elements[:]
+                next.remove(e)
+
+                prev.append(e)
+                dfs(next)
+                prev.pop()
+
+        dfs(nums)
+        return results
+
+nums = [1, 2, 3]
+sol = Solution()
+print(sol.permute(nums))
+
